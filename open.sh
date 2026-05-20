0d7e054d8f51131cb7a6694400be8ae153ec16cc
Jason Klein <jklein.64@verizon.net>
2026-05-20 16:31:10 -0400

    style: remove dead code

--- open.sh
+++ open.sh
@@ -8,11 +8,3 @@ docker run --rm -it --init \
   -v "$(pwd)":/root \
   --privileged \
   spriter
-# xhost +localhost
-# STATE=$(docker container ls --all --filter 'name=spriter' --format '{{ .State }}')
-# if [[ "$STATE" != "running" ]]; then
-#   docker start spriter
-# fi
-# docker exec -it -v $(pwd)/:/root spriter '/root/SpriterR10(64)/Spriter'
-# docker container create -it --name spriter --platform linux/x86_64 --net=host -e DISPLAY=host.docker.internal:0 -v /tmp/.X11-unix:/tmp/.X11-unix:rw --privileged spriter
-# docker run --rm -it --platform linux/x86_64 --net=host -e DISPLAY=host.docker.internal:0 -v /tmp/.X11-unix:/tmp/.X11-unix:rw --privileged spriter /bin/bash
